fd13b231152e5b9a706b4a42334897085f3e5879
Matvey <sinema3d00@gmail.com>
2024-05-24 21:50:59 +0300

    made score logic

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Score : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private const string HIGHSCORE = "Highscore";
+
+    public static Score Instance;
+
+    [SerializeField] private TextMeshProUGUI _currentScoreText;
+    [SerializeField] private TextMeshProUGUI _highestScoreText;
+
+    private int _score;
+    private void Awake()
     {
-        
+        if(Instance == null)
+        {
+            Instance = this;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        _currentScoreText.text = _score.ToString();
+
+        _highestScoreText.text = PlayerPrefs.GetInt(HIGHSCORE, 0).ToString();
+
+        UpdateHighScore();
+
+    }
+
+    private void UpdateHighScore()
+    {
+        if (_score > PlayerPrefs.GetInt(HIGHSCORE))
+        {
+            PlayerPrefs.SetInt(HIGHSCORE, _score);
+            _highestScoreText.text = _score.ToString();
+        }
+    }
+
+    public void UpdateScore()
+    {
+        _score++;
+        _currentScoreText.text = _score.ToString();
+        UpdateHighScore();
     }
 }
