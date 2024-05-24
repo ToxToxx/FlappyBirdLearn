258499a83efcb7858ffc20cb75721b951eb8ac38
Matvey <sinema3d00@gmail.com>
2024-05-24 16:30:52 +0300

    made flying behavior

---
+++ Assets/Scripts/FlyBehavior.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class FlyBehavior : MonoBehaviour
+{
+    [SerializeField] private float _velocity = 1.5f;
+    [SerializeField] private float _rotationSpeed = 10f; = 
+
+    private Rigidbody2D _birdRb;
+
+    private void Start()
+    {
+        _birdRb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if(Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            _birdRb.velocity = Vector2.up * _velocity;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, _birdRb.velocity.y * _rotationSpeed); 
+    }
+}
